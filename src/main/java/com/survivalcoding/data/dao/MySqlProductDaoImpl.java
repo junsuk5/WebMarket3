d828fcbd25a99f8eb95f91657cf6b70f823eb487
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-06-15 14:30:50 +0900

    상품 등록 DB 연결 성공

--- src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
+++ src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
@@ -2,6 +2,7 @@ package com.survivalcoding.data.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,39 +25,54 @@ public class MySqlProductDaoImpl implements ProductDao {
         List<Product> results = new ArrayList<>();
 
         String sql = "SELECT * FROM product";
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:52000/myDB", "kim", "json");
-                Statement stmt = conn.createStatement();) {
-            try (ResultSet rs = stmt.executeQuery(sql)) {
-                while (rs.next()) {
-                    String id = rs.getString("p_id");
-                    String name = rs.getString("p_name");
-                    int unitPrice = rs.getInt("p_unitPrice");
-                    String description = rs.getString("p_description");
-                    String category = rs.getString("p_category");
-                    String manufacturer = rs.getString("p_manufacturer");
-                    long unitsInStock = rs.getLong("p_unitsInStock");
-                    String condition = rs.getString("p_condition");
+        try (Connection conn =
+                DriverManager.getConnection("jdbc:mysql://localhost:52000/myDB", "kim", "json");
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);) {
+            while (rs.next()) {
+                String id = rs.getString("p_id");
+                String name = rs.getString("p_name");
+                int unitPrice = rs.getInt("p_unitPrice");
+                String description = rs.getString("p_description");
+                String category = rs.getString("p_category");
+                String manufacturer = rs.getString("p_manufacturer");
+                long unitsInStock = rs.getLong("p_unitsInStock");
+                String condition = rs.getString("p_condition");
 
-                    Product product = new Product(id, name, unitPrice);
-                    product.setDescription(description);
-                    product.setCategory(category);
-                    product.setManufacturer(manufacturer);
-                    product.setUnitsInStock(unitsInStock);
-                    product.setCondition(condition);
+                Product product = new Product(id, name, unitPrice);
+                product.setDescription(description);
+                product.setCategory(category);
+                product.setManufacturer(manufacturer);
+                product.setUnitsInStock(unitsInStock);
+                product.setCondition(condition);
 
-                    results.add(product);
-                }
+                results.add(product);
             }
         } catch (SQLException e) {
-            throw new IllegalStateException("db 연결 실패");
+            throw new IllegalStateException("db 연결 실패" + e.getMessage());
         }
         return results;
     }
 
     @Override
     public void insert(Product product) {
-        // TODO Auto-generated method stub
-
+        // PreparedStatement 동적 쿼리 생성: Insert, update, delete 여러번 할 때 고속
+        String sql = "INSERT INTO product VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn =
+                DriverManager.getConnection("jdbc:mysql://localhost:52000/myDB", "kim", "json");
+                PreparedStatement stmt = conn.prepareStatement(sql);) {
+            stmt.setString(1, product.getId());
+            stmt.setString(2, product.getName());
+            stmt.setInt(3, product.getUnitPrice());
+            stmt.setString(4, product.getDescription());
+            stmt.setString(5, product.getCategory());
+            stmt.setString(6, product.getManufacturer());
+            stmt.setLong(7, product.getUnitsInStock());
+            stmt.setString(8, product.getCondition());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+           throw new IllegalStateException("insert 실패 " + e.getMessage());
+        }
     }
 
     @Override
